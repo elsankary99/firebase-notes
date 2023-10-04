44e34cd4e0b0e2abc3d5c9df504c240a7168dd83
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-05 00:53:20 +0300

    refactor add note page

--- lib/view/Home/pages/add_note_page.dart
+++ lib/view/Home/pages/add_note_page.dart
@@ -1,40 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fb_note/core/constant/app-colors.dart';
-import 'package:fb_note/core/constant/app_text_style.dart';
-import 'package:fb_note/core/extension/media_query.dart';
 import 'package:fb_note/core/widget/custom_circle_indicator.dart';
 import 'package:fb_note/core/widget/custom_toast.dart';
 import 'package:fb_note/provider/auth/note_provider/notes_provider.dart';
 import 'package:fb_note/view/widget/home_widget/add_note_fields.dart';
+import 'package:fb_note/view/widget/home_widget/pick_note_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 @RoutePage()
-class AddNotePage extends ConsumerStatefulWidget {
+class AddNotePage extends ConsumerWidget {
   const AddNotePage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _AddNotePageState();
-}
-
-class _AddNotePageState extends ConsumerState<AddNotePage> {
-  int currentIndex = 0;
-  bool open = false;
-  List<Color> colors = [
-    const Color(0xff003f7d),
-    const Color(0xffff8e00),
-    const Color(0xff002347),
-    const Color(0xfffd7702),
-    const Color(0xff003366),
-    const Color(0xffff5003),
-  ];
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final provider = ref.read(notesProvider.notifier);
     final state = ref.watch(notesProvider);
-    ref.read(notesProvider.notifier).color = colors[currentIndex].value;
     ref.listen(
       notesProvider,
       (previous, currentState) {
@@ -75,69 +58,7 @@ class _AddNotePageState extends ConsumerState<AddNotePage> {
                         )),
                   ],
                 ),
-                SliverToBoxAdapter(
-                    child: SizedBox(
-                  height: open ? context.height * 0.1 : null,
-                  child: Column(
-                    children: [
-                      open
-                          ? Expanded(
-                              child: ListView.builder(
-                                physics: const BouncingScrollPhysics(),
-                                itemCount: colors.length,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10),
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        setState(() {
-                                          currentIndex = index;
-                                        });
-                                      },
-                                      child: CircleAvatar(
-                                        radius: 25.r,
-                                        backgroundColor: colors[index],
-                                        child: currentIndex == index
-                                            ? const Icon(
-                                                FontAwesomeIcons.check,
-                                              )
-                                            : null,
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            )
-                          : const SizedBox(),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Select color ...",
-                              style: AppTextStyle.lato400Style18,
-                            ),
-                            InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    open = !open;
-                                  });
-                                },
-                                child: Icon(
-                                  open
-                                      ? FontAwesomeIcons.chevronUp
-                                      : FontAwesomeIcons.chevronDown,
-                                  color: AppColors.orange,
-                                ))
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                )),
+                const SliverToBoxAdapter(child: PickNoteColor()),
                 const SliverToBoxAdapter(child: AddNoteFelids()),
               ],
             ),
---
+++ lib/view/widget/home_widget/pick_note_color.dart
@@ -0,0 +1,93 @@
+import 'package:fb_note/core/constant/app-colors.dart';
+import 'package:fb_note/core/constant/app_text_style.dart';
+import 'package:fb_note/core/extension/media_query.dart';
+import 'package:fb_note/provider/auth/note_provider/notes_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PickNoteColor extends ConsumerStatefulWidget {
+  const PickNoteColor({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _PickNoteColorState();
+}
+
+class _PickNoteColorState extends ConsumerState<PickNoteColor> {
+  int currentIndex = 0;
+  bool open = false;
+  List<Color> colors = [
+    const Color(0xff003f7d),
+    const Color(0xffff8e00),
+    const Color(0xff002347),
+    const Color(0xfffd7702),
+    const Color(0xff003366),
+    const Color(0xffff5003),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    ref.read(notesProvider.notifier).color = colors[currentIndex].value;
+    return SizedBox(
+      height: open ? context.height * 0.1 : null,
+      child: Column(
+        children: [
+          open
+              ? Expanded(
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: colors.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              currentIndex = index;
+                            });
+                          },
+                          child: CircleAvatar(
+                            radius: 25.r,
+                            backgroundColor: colors[index],
+                            child: currentIndex == index
+                                ? const Icon(
+                                    FontAwesomeIcons.check,
+                                  )
+                                : null,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              : const SizedBox(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Select color ...",
+                  style: AppTextStyle.lato400Style18,
+                ),
+                InkWell(
+                    onTap: () {
+                      setState(() {
+                        open = !open;
+                      });
+                    },
+                    child: Icon(
+                      open
+                          ? FontAwesomeIcons.chevronUp
+                          : FontAwesomeIcons.chevronDown,
+                      color: AppColors.orange,
+                    ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
