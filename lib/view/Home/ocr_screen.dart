25c4e5f28841f576341ceb9641b88fbb6e9dd66d
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-05 13:21:52 +0300

    image to text created successfuly

--- lib/view/Home/ocr_screen.dart
+++ lib/view/Home/ocr_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:fb_note/core/constant/app-colors.dart';
 import 'package:fb_note/core/constant/app_strings.dart';
 import 'package:fb_note/core/constant/app_text_style.dart';
@@ -6,12 +9,46 @@ import 'package:fb_note/core/widget/custom_orange_buton.dart';
 import 'package:fb_note/view/widget/home_widget/clear_iamge_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
-class OCRScreen extends ConsumerWidget {
+class OCRScreen extends ConsumerStatefulWidget {
   const OCRScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _OCRScreenState();
+}
+
+class _OCRScreenState extends ConsumerState<OCRScreen> {
+  File? _image;
+  String title = "";
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      setState(() {
+        _image = File(image.path);
+      });
+    } catch (e) {
+      log("===image==${e.toString()}=======");
+    }
+  }
+
+  Future textRecognition(File image) async {
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final inputImage = InputImage.fromFilePath(image.path);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    setState(() {
+      title = recognizedText.text;
+    });
+    log("==path===$title=====");
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
@@ -29,23 +66,54 @@ class OCRScreen extends ConsumerWidget {
               children: [
                 Container(
                   height: context.height * 0.3,
+                  width: double.infinity,
                   decoration: BoxDecoration(
+                      color: AppColors.lightGrey,
+                      image: _image != null
+                          ? DecorationImage(
+                              image: FileImage(_image!), fit: BoxFit.fill)
+                          : null,
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(color: AppColors.orange, width: 2)),
+                  child: _image == null
+                      ? Icon(
+                          Icons.add_a_photo_outlined,
+                          size: 45.sp,
+                          color: AppColors.orange,
+                        )
+                      : null,
                 ),
                 SizedBox(height: context.height * 0.02),
                 Container(
                   height: context.height * 0.3,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(15),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(color: AppColors.orange, width: 2)),
+                  child: ListView(
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      SelectableText(
+                        title,
+                        style: AppTextStyle.lato400Style18,
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(height: context.height * 0.05),
                 SizedBox(
                     height: context.height * 0.05,
                     child: Row(
                       children: [
-                        ClearImageButton(onPressed: () {}),
+                        //! glary
+                        ClearImageButton(onPressed: () {
+                          pickImage(ImageSource.gallery).then((value) {
+                            if (_image != null) {
+                              textRecognition(_image!);
+                            }
+                          });
+                        }),
                         SizedBox(width: context.width * 0.05),
                         CustomOrangeButton(
                           text: AppStrings.scanImage,
