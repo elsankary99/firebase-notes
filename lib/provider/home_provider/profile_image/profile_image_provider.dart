b10fb1739d3dcad0014a024b9b2a18809cd78c6e
Ahmed Elsankary <elsankary99@gmail.com>
2023-10-09 20:42:16 +0300

    pick user image part 2

--- lib/provider/home_provider/profile_image/profile_image_provider.dart
+++ lib/provider/home_provider/profile_image/profile_image_provider.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
 
 part 'profile_image_state.dart';
 
@@ -17,6 +19,8 @@ class ProfileImageProvider extends StateNotifier<ProfileImageState> {
   File? image;
   String title = "";
   String? imageUrl;
+  final userImage = FirebaseFirestore.instance.collection("userImage");
+
   Future<void> pickProfileImage(ImageSource source) async {
     state = ProfileImageLoading();
 
@@ -25,14 +29,34 @@ class ProfileImageProvider extends StateNotifier<ProfileImageState> {
       if (imagePath == null) return;
       image = File(imagePath.path);
       if (image != null) {
-        final imageName = basename(imagePath.path);
-        final storageRef = FirebaseStorage.instance.ref(imageName);
+        // final imageName = basename(imagePath.path);
+        final storageRef = FirebaseStorage.instance
+            .ref(FirebaseAuth.instance.currentUser!.uid);
         await storageRef.putFile(image!);
         imageUrl = await storageRef.getDownloadURL();
+        await saveImage(imageUrl!);
       }
       state = ProfileImageSuccess();
     } catch (e) {
+      log("======${e.toString()}=======");
+
       state = ProfileImageError(e.toString());
     }
   }
+
+  saveImage(String url) async {
+    try {
+      await userImage
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set({'image': url});
+    } catch (e) {
+      log("======${e.toString()}=======");
+    }
+  }
 }
+
+final getImageProvider = FutureProvider<String?>((ref) async {
+  final userImage = FirebaseFirestore.instance.collection("userImage");
+  final url = await userImage.doc(FirebaseAuth.instance.currentUser!.uid).get();
+  return url.data()!["image"];
+});
--- lib/view/widget/home_widget/user_information.dart
+++ lib/view/widget/home_widget/user_information.dart
@@ -1,7 +1,10 @@
+import 'package:fb_note/core/constant/app_images.dart';
+import 'package:fb_note/core/router/app_router.dart';
+import 'package:fb_note/core/widget/custom_circle_indicator.dart';
+import 'package:fb_note/core/widget/custom_toast.dart';
 import 'package:fb_note/provider/home_provider/profile_image/profile_image_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fb_note/core/constant/app-colors.dart';
-import 'package:fb_note/core/constant/app_images.dart';
 import 'package:fb_note/core/extension/media_query.dart';
 import 'package:fb_note/core/widget/custom_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,7 +20,9 @@ class UserInformation extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final provider = ref.read(profileImageProvider.notifier);
+
     ref.watch(profileImageProvider);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
       child: Row(children: [
@@ -25,12 +30,27 @@ class UserInformation extends ConsumerWidget {
           width: context.width * 0.24,
           child: Stack(
             children: [
-              CircleAvatar(
-                radius: 40.r,
-                backgroundImage: provider.imageUrl != null
-                    ? NetworkImage(provider.imageUrl!)
-                    : null,
-                // foregroundImage: const AssetImage(Assets.assetsImagesUser),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100.r),
+                child: SizedBox(
+                  height: context.height * 0.12,
+                  width: context.height * 0.16,
+                  child: ref.watch(getImageProvider).when(
+                        skipLoadingOnRefresh: false,
+                        data: (data) => data != null
+                            ? Image.network(
+                                data,
+                                fit: BoxFit.fill,
+                              )
+                            : Image.asset(Assets.assetsImagesUser),
+                        error: (error, stackTrace) => customToast(
+                            title: error.toString(), color: Colors.red),
+                        loading: () => const CustomCircleIndicator(
+                          color: AppColors.orange,
+                        ),
+                      ),
+                  // foregroundImage: const AssetImage(Assets.assetsImagesUser),
+                ),
               ),
               Positioned(
                   right: -0.01,
@@ -39,8 +59,12 @@ class UserInformation extends ConsumerWidget {
                     onTap: () {
                       getImage(context, cameraBtn: () async {
                         await provider.pickProfileImage(ImageSource.camera);
+                        router.pop();
+                        ref.invalidate(getImageProvider);
                       }, galleryBtn: () async {
                         await provider.pickProfileImage(ImageSource.gallery);
+                        router.pop();
+                        ref.invalidate(getImageProvider);
                       });
                     },
                     child: CircleAvatar(
